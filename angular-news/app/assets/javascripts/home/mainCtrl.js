223661be8430c7efc0f543b6fd43115484ddf62e
Kyle Henson <khenson.kh@gmail.com>
2015-10-16 14:29:18 -0600

    Posts and Comments are now wired to the models

--- angular-news/app/assets/javascripts/home/mainCtrl.js
+++ angular-news/app/assets/javascripts/home/mainCtrl.js
@@ -3,22 +3,19 @@ angularNews.controller('MainCtrl', [
   'posts',
   function($scope, posts){
     $scope.posts = posts.posts;
+
     $scope.addPost = function(){
       if(!$scope.title || $scope.title === '') { return; }
-      $scope.posts.push({
+      posts.create({
         title: $scope.title,
         link: $scope.link,
-        upvotes: 0,
-        comments: [
-          {author: 'Me', body: 'good stuff', upvotes: 0},
-          {author: 'You', body: 'lets go to mars', upvotes: 0}
-        ]
       });
       $scope.title = '';
       $scope.link = '';
     };
+
     $scope.incrementUpvotes = function(post){
-      post.upvotes += 1;
+      posts.upvote(post);
     };
   }
 ])
